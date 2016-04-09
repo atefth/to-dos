1abfe0e4cc3ba27e78bc157a9b73f0e640beaade
Atef Haque <atefth@gmail.com>
2016-04-09 18:50:23 +0430

    updated package.json and server.js for openshift deployment

--- server.js
+++ server.js
@@ -79,6 +79,10 @@ app.get('*', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(server_port, server_ip_address, function () {
+      console.log( "Listening on " + server_ip_address + ", server_port " + port  )
+});
